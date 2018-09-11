aa42ba0bf81edc45a8df158241193f683304f55a
yulifu <yulifu@douyu.tv>
2018-09-11 14:17:53 +0800

    change js

--- web/index.php
+++ web/index.php
@@ -17,43 +17,12 @@ $_G['seo'] = [
 ];
 $_G['nowTime'] = isset($_SERVER['REQUEST_TIME']) ? $_SERVER['REQUEST_TIME'] : time();
 $_G['theme'] = 'y-theme-afternoon';
-$_G['nav'] = [
-    [
-        'id' => 1,
-        'pid' => 0,
-        'title' => '动漫',
-        'navurl' => 'anime',
-        'listorder' => 0,
-        'linktype' => 1  // 1 本站链接 2 站外链接
-    ],
-    [
-        'id' => 2,
-        'pid' => 0,
-        'title' => '游戏',
-        'navurl' => 'game',
-        'listorder' => 0,
-        'linktype' => 1
-    ],
-    [
-        'id' => 3,
-        'pid' => 0,
-        'title' => '鲜事',
-        'navurl' => 'around',
-        'listorder' => 0,
-        'linktype' => 1
-    ],
-    [
-        'id' => 4,
-        'pid' => 0,
-        'title' => 'Cosplay',
-        'navurl' => 'cosplay',
-        'listorder' => 0,
-        'linktype' => 1
-    ]
-];
 
 require(__DIR__ . '/../../fateblog-share/vendor/afuafuyo/fatephp/Fate.php');
 include __DIR__ . '/../../fateblog-share/core/common.php';
+
+$agent = $_SERVER['HTTP_USER_AGENT'];
+$_G['isMobile'] = (strpos($agent, 'iPhone') || strpos($agent, 'Android')) ? true : false;
 
 $res = (new fate\web\Application([
     // 加密用
@@ -65,23 +34,15 @@ $res = (new fate\web\Application([
     'routesMap' => [
         'anime' => [
             'classPath' => 'app\controllers\classify\IndexController',
-            'viewPath' => 'classify',
             'navid' => 1
         ],
         'game' => [
             'classPath' => 'app\controllers\classify\IndexController',
-            'viewPath' => 'classify',
             'navid' => 2
         ],
         'around' => [
             'classPath' => 'app\controllers\classify\IndexController',
-            'viewPath' => 'classify',
             'navid' => 3
-        ],
-        'cosplay' => [
-            'classPath' => 'app\controllers\classify\IndexController',
-            'viewPath' => 'classify',
-            'navid' => 4
         ]
     ],
     'modules' => [
